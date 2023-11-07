c97696ce39325e75744ef97c338184c8e8ad00cd
pruzanov <pruzanov@oicr.on.ca>
2023-11-07 13:32:06 -0500

    Fixing issues with incorrect splitting of strings

--- txt-to-json.py
+++ txt-to-json.py
@@ -1,17 +1,18 @@
 import json
 import argparse
+import re
+
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-n","--nameFile", required=True)
-parser.add_argument("-e","--enrichmentFile", required=True)
-parser.add_argument("-c","--coverageCountsFile", required=True)
-parser.add_argument("-w","--coverageWindowsFile", required=True)
-parser.add_argument("-s","--saturationMetricsFile", required=True)
-parser.add_argument("-d","--dedupMetricsFile", required=True)
-parser.add_argument("-u","--summaryFile", required=True)
-parser.add_argument("-a","--alignmentFile", required=True)
-parser.add_argument("-t","--thaliaFile", required=True)
-
+parser.add_argument("-n", "--nameFile", required=True)
+parser.add_argument("-e", "--enrichmentFile", required=True)
+parser.add_argument("-c", "--coverageCountsFile", required=True)
+parser.add_argument("-w", "--coverageWindowsFile", required=True)
+parser.add_argument("-s", "--saturationMetricsFile", required=True)
+parser.add_argument("-d", "--dedupMetricsFile", required=True)
+parser.add_argument("-u", "--summaryFile", required=True)
+parser.add_argument("-a", "--alignmentFile", required=True)
+parser.add_argument("-t", "--thaliaFile", required=True)
 
 
 args = parser.parse_args()
@@ -39,8 +40,9 @@ with open(args.dedupMetricsFile, "r+") as dedupFile:
             if line != '\n':
                 dedupFile.write(line)
     dedupFile.truncate()
+
 with open(args.dedupMetricsFile, "r+") as dedupFileNew:
-    dedup = (dedupFileNew.read().split())
+    dedup = re.split('\t|\n', dedupFileNew.read())
 
 
 with open(args.summaryFile, "r+") as summaryFile:
@@ -52,7 +54,7 @@ with open(args.summaryFile, "r+") as summaryFile:
                 summaryFile.write(line)
     summaryFile.truncate()
 with open(args.summaryFile, "r+") as summaryFileNew:
-    summary = (summaryFileNew.read().split())
+    summary = re.split('\t|\n', summaryFileNew.read())
 
 with open(args.alignmentFile, "r+") as alignmentFile:
     new_f = alignmentFile.readlines()
@@ -63,7 +65,7 @@ with open(args.alignmentFile, "r+") as alignmentFile:
                 alignmentFile.write(line)
     alignmentFile.truncate()
 with open(args.alignmentFile, "r+") as alignmentFileNew:
-    alignment = (alignmentFileNew.read().split())
+    alignment = re.split('\t|\n', alignmentFileNew.read())
 
 with open(args.thaliaFile, "r") as thaliaFile:
     thalia = (thaliaFile.read().split())
@@ -86,32 +88,26 @@ for x in range(0,4):
     data[saturation[0+x]] = saturation[5+x]
 
 dedup[0] = "LIBRARY_0"
-data[dedup[0]] = (dedup[10]+"_"+dedup[11])
-for x in range(0,9):
-    data[dedup[1+x]] = dedup[12+x]
+for x in range(0, 10):
+    data[dedup[x]] = dedup[10 + x]
 if data[dedup[9]] == "set_size":
     data[dedup[9]] = ""
+data[dedup[0]] = data[dedup[0]].replace(' ', '_')
 
-
-data[summary[0]] = (summary[15]+"_"+summary[16])
-for x in range(0,11):
-    data[summary[1+x]] = summary[17+x]
 summary[12] = "SAMPLE_0"
 summary[13] = "LIBRARY_1"
 summary[14] = "READ_GROUP_0"
-for x in range(11,14):
-    data[summary[1 + x]] = ""
+for x in range(0, 15):
+    data[summary[x]] = summary[15+x]
+data[summary[0]] = data[summary[0]].replace(' ', '_')
 
-for x in range(0,23):
-    data[alignment[0+x]] = alignment[27+x]
 alignment[22] = "SAMPLE_1"
 alignment[23] = "LIBRARY_2"
 alignment[24] = "READ_GROUP_1"
-for x in range(21,24):
-    data[alignment[1 + x]] = ""
+for x in range(0, 25):
+    data[alignment[x]] = alignment[27+x]
 
-
-for x in range(0,6):
+for x in range(0, 6):
     data[thalia[0+x]] = thalia[6+x]
 
 with open('qc_metrics.json', 'w') as f:
